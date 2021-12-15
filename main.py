edbaa4e11ebd26b174cb56368a73298311b4139b
luisgdov1 <55296930+luisgdov1@users.noreply.github.com>
2021-12-15 16:29:46 -0600

    NINGUNO

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Util.Padding import pad, unpad
-
+from Crypto.Hash import SHA1
+from Crypto.Signature import pss
 
 # OPERACIONES BASICAS
 def txtToBytes(rutaArchivoTxt):
@@ -12,13 +13,10 @@ def txtToBytes(rutaArchivoTxt):
     file.close()
     return bytes
 
-def leerLlavePublica(rutaLlavePublica):
+def leerLlave(rutaLlavePublica):
     llavePublica = RSA.import_key(open(rutaLlavePublica).read())
     return llavePublica
 
-def leerLlavePrivada(rutaLlavePrivada):
-    llavePrivada = RSA.import_key(open(rutaLlavePrivada).read())
-    return llavePrivada
 
 def generarLlave():
     caracteres = list(string.ascii_letters + string.digits)
@@ -36,80 +34,102 @@ def generarArchivo (rutaArchivoFA, bytes):
     file.close()
     print("Archivo nuevo generado")
 
-#CIFRADO y FIRMA
-def cifrarTXT (rutaArchivoTxt, rutaLlavePublica, rutaArchivoNuevoFA):
-    bytesOG = txtToBytes(rutaArchivoTxt)
-    llave_str = generarLlave()
-    llave_b = bytes(llave_str, 'utf-8')
-    iv_b = bytes(llave_str[::-1], 'utf-8')
+##SOLO FIRMA
+def firmar(rutaLlavePrivada, rutaMensajeClaro, rutaNuevoMensajeFirmado):
+    mensaje = txtToBytes(rutaMensajeClaro)
+    key = leerLlave(rutaLlavePrivada)
+    hash = SHA1.new(mensaje)
+    firmado = pss.new(key).sign(hash)
+    contenido = firmado + hash
+    generarArchivo(rutaNuevoMensajeFirmado, contenido)
+
+def verificarFirma(rutaLlavePublica, rutaMensajeFirmado):
+    mensaje = txtToBytes(rutaMensajeFirmado)
+    key = leerLlave(rutaLlavePublica)
+    h = SHA1.new(mensaje[128:])
+    verificar = pss.new(key)
+    try:
+        verificar.verify(h, mensaje[0:128])
+        print("Firma autentica")
+    except():
+        print("Firma no valida")
+
+###SOLO AES
+def cifradoAES (rutaMensajeClaro, rutaNuevoMensajeCifrado):
+    byteog = txtToBytes(rutaMensajeClaro)
+    llave_crudo = generarLlave()
+    llave_b = bytes(llave_crudo, 'utf-8')
+    iv_b = llave_b[::-1]
     cifradoCBC = AES.new(llave_b, AES.MODE_CBC, iv_b)
-    bytesCifrado = cifradoCBC.encrypt(pad(bytesOG, AES.block_size))
-    bytesParametros = cifradoRSA1(rutaLlavePublica, llave_b)
-    print(len(bytesParametros))
-    print(len(bytesCifrado))
-    bytesTotales = bytesParametros + bytesCifrado
-    print(len(bytesTotales))
-    generarArchivo(rutaArchivoNuevoFA, bytesTotales)
+    bytesCifrado = cifradoCBC.encrypt(pad(byteog, AES.block_size))
+    generarArchivo(rutaNuevoMensajeCifrado, bytesCifrado)
 
+def descifradoAES (rutaMensajeCifrado, llave, rutaNuevoMensajeCifrado):
+    bytesorigen = txtToBytes(rutaMensajeCifrado)
+    llave_b = bytes(llave, 'utf8')
+    iv_b = llave_b[::-1]
+    cifradoCBC = AES.new(llave, AES.MODE_CBC, iv_b)
+    bytesdescifrado = unpad(cifradoCBC.decrypt(bytesorigen), AES.block_size)
+    generarArchivo(rutaNuevoMensajeCifrado, bytesdescifrado)
 
-def cifradoRSA1(rutaLlavePublica, parametros):
-    llavePublica = leerLlavePublica(rutaLlavePublica)
-    cipher = PKCS1_OAEP.new(llavePublica)
-    mensajecifrado = cipher.encrypt(parametros)
-    return mensajecifrado
-
-#DESCIFRADO Y FIRMA
-def descifrarTXT (rutaMensajeCifrado, rutaLlavePrivada):
-    bytesCF = txtToBytes(rutaMensajeCifrado)
-    parametrosBytes = descifradoRSA1(rutaLlavePrivada, bytesCF[0:128])
-    contenido = bytesCF[128:]
-    llave = parametrosBytes
-    iv = parametrosBytes[::-1]
-    descifradoCBC = AES.new(llave, AES.MODE_CBC, iv)
-    bytesDescifrados = unpad(descifradoCBC.decrypt(contenido), AES.block_size)
-    print(bytesDescifrados.decode('utf8'))
-
-def descifradoRSA1(rutaLlavePrivada, parametros):
-    llavePrivada = leerLlavePrivada(rutaLlavePrivada)
-    cipher = PKCS1_OAEP.new(llavePrivada)
-    parametros_bytes = cipher.decrypt(parametros)
-    print(parametros_bytes.decode('utf-8'))
-    return parametros_bytes
-
-
-##SOLO AES CIFRADO
-def cifradoAES(rutaArchivoTXT, rutaArchivoCifrado):
-    bytesOG = txtToBytes(rutaArchivoTXT)
+##AMBOS FIRMA + AES
+'''FUNCIONES AUXILIARES'''
+def cifradoAESFIRMA(rutaMensajeClaro, rutaLlavePublica, rutaLlavePrivada, rutaNuevoMensajeCifrado):
+    #Leer bytes del txt original
+    bytesoriginales = txtToBytes(rutaMensajeClaro)
+    #GENERAR PARAMETROS
     llave_str = generarLlave()
-    llave_b = bytes(llave_str, 'utf-8')
+    llave_b = bytes(llave_str, 'utf8')
     iv_b = llave_b[::-1]
-    cifradoAES = AES.new(llave_b, AES.MODE_CBC, iv_b)
-    bytesCifrado = cifradoAES.encrypt(pad(bytesOG, AES.block_size))
-    generarArchivo(rutaArchivoCifrado, bytesCifrado)
+    ##Ciframos los parametros con la llave Publica.
+    llavePublica = leerLlave(rutaLlavePublica)
+    cifradorParametros = PKCS1_OAEP.new(llavePublica)
+    cifradoParametrosBytes = cifradorParametros.encrypt(llave_b)
+    print(len(cifradoParametrosBytes))
 
-#SOLO AES DESCIFRADO
-def descifradoAES(rutaMnesajeCifrado, llave, rutaMensajeClaro):
-    bytesOG = txtToBytes(rutaMnesajeCifrado)
-    llave_b = bytes(llave, 'utf-8')
-    iv_b = llave_b[::-1]
-    decifrado  = AES.new(llave_b, AES.MODE_CBC, iv_b)
-    bytesDES = unpad(decifrado.decrypt(bytesOG), AES.block_size)
-    print(bytesDES.decode("utf-8"))
-    generarArchivo(rutaMensajeClaro, bytesDES)
+    ##Firmamos el texto y guardamos
+    llavePrivada = leerLlave(rutaLlavePrivada)
+    hash_contenido = SHA1.new(bytesoriginales)
+    firmado = pss.new(llavePrivada).sign(hash_contenido)
+    firmatotal = firmado + bytesoriginales
+    print (len(firmado))
+    print(len(firmatotal))
+    ##Ciframos el guardado
+    cifradoCBC = AES.new(llave_b, AES.MODE_CBC, iv_b)
+    bytesCifrados = cifradoCBC.encrypt(pad(firmatotal, AES.block_size))
 
-#CIFRADO CON RSA
-def cifradoRSA(rutaMensajeClaro, rutaLlavePublica, rutaNuevoMensajeCifrado):
-    llavep = leerLlavePublica(rutaLlavePublica)
-    bytesOG = txtToBytes(rutaMensajeClaro)
-    cipher = PKCS1_OAEP.new(llavep)
-    bytesCIF = cipher.encrypt(bytesOG)
-    generarArchivo(rutaNuevoMensajeCifrado, bytesCIF)
+    bytesTotales = cifradoParametrosBytes + bytesCifrados
 
-def descifradoRSA (rutaMensajeCifrado, rutaLlavePrivada, rutaNuevoMensajeClaro):
-    llavepriv = leerLlavePrivada(rutaLlavePrivada)
-    bytesCIF = txtToBytes(rutaMensajeCifrado)
-    descifrado = PKCS1_OAEP.new(llavepriv)
-    mensaje = descifrado.decrypt(bytesCIF)
-    generarArchivo(rutaNuevoMensajeClaro, mensaje)
+    generarArchivo(rutaNuevoMensajeCifrado, bytesTotales)
 
-'''Todo tiene que ir dentro de un try'''
+
+def descifrarAESFIRMA(rutaMensajeCifrado, rutaLlavePublica, rutaLlavePrivada, rutaNuevoMensajeDescifrado):
+    contenido = txtToBytes(rutaMensajeCifrado)
+    llavePrivada = leerLlave(rutaLlavePrivada)
+    cipherRSA = PKCS1_OAEP.new(llavePrivada)
+    llave_RSAb = cipherRSA.decrypt(contenido[0:128])
+    iv_b = llave_RSAb[::-1]
+    ##Desciframos lo firmado con AES:
+
+    descifradoAESRSA = AES.new(llave_RSAb, AES.MODE_CBC, iv_b)
+    bytesFIRMACIF = unpad(descifradoAESRSA.decrypt(contenido[128:]), AES.block_size)
+
+    #Leemos primero las credenciales
+    key = leerLlave(rutaLlavePublica)
+    h = SHA1.new(bytesFIRMACIF[128:])
+    verificar = pss.new(key)
+    try:
+        verificar.verify(h,bytesFIRMACIF[0:128])
+        generarArchivo(rutaNuevoMensajeDescifrado, bytesFIRMACIF[128:])
+        print("CONTENIDO DEL MENSAJE..............................................................")
+        print(bytesFIRMACIF[128:].decode('utf-8'))
+        print("FIN DEL CONTENIDO")
+        print("VERIFICADO")
+    except:
+        print("NO VERIDICO")
+
+
+
+
+cifradoAESFIRMA('mensajeAlicia.txt', 'llavepublicaAlicia.der', 'llaveprivadaBetito.der', 'cifradoAliciaBetito.txt')
+descifrarAESFIRMA('cifradoAliciaBetito.txt', 'llavepublicaBetito.der', 'llaveprivadaAlicia.der', 'nuevonuevomensaje.txt')
